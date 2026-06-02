3984221fa6cf8113dcc5e3983949ce69c7418fb8
Rémi Beaufils <remi.beaufils@claranet.com>
2026-06-02 16:01:13 +0200

    fix: 🐛 default output `resource` now require sensitive with OpenTofu 1.12+

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "resource" {
   description = "Privileged Identity Management resource object."
   value       = azuread_directory_role_eligibility_schedule_request.main
+  sensitive   = true
 }
